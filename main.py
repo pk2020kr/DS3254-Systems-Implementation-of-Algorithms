973f52f84c5e6419481c9bc18574d6246683f165
Prabhat <pk2020kr@gmail.com>
2025-01-03 21:54:37 +0530

    Mac system information

---
+++ main.py
@@ -0,0 +1,20 @@
+# for Mac system information
+!system_profiler SPHardwareDataType;
+'''
+Hardware:
+
+    Hardware Overview:
+
+      Model Name: MacBook Air
+      Model Identifier: Mac14,2
+      Model Number: MLXW3HN/A
+      Chip: Apple M2
+      Total Number of Cores: 8 (4 performance and 4 efficiency)
+      Memory: 8 GB
+      System Firmware Version: 11881.61.3
+      OS Loader Version: 11881.61.3
+      Serial Number (system): D4QJ14DNJF
+      Hardware UUID: FB39C139-85B4-51E1-9B4D-57867ED5A68A
+      Provisioning UDID: 00008112-000838E02EA3401E
+      Activation Lock Status: Disabled
+'''
